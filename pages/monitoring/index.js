c57ba7a18d82b124e67ffbc6df5c6ada87ffbace
salarabbaspour <Sa19112118>
2022-02-16 17:32:01 +0330

    comit

---
+++ pages/monitoring/index.js
@@ -0,0 +1,79 @@
+
+import { useEffect, useState } from 'react';
+import { AreaChart, CartesianGrid ,XAxis,Tooltip,Area,YAxis,ResponsiveContainer,ReferenceLine} from 'recharts';
+import Pusher from 'pusher-js'
+
+export default function Monitoring({t}) {
+    const pusher = new Pusher("ebbfc5d80c211fb6817b", {
+        cluster: "eu",
+      });
+    const channel = pusher.subscribe("my-channel");
+  const [data,setData]=useState([]);
+	const [num,setNum]=useState(1);
+	const[ref,setRef]=useState(false);
+	const [max,setMax] =useState(6);
+	const [loading,setLoading]=useState(true)
+    channel.bind("my-event", (data) => {
+        setNum(data.value)
+    });
+useEffect(()=>{
+  if(t!=undefined) setData(t);
+ 
+},[])
+const setDate=()=>{
+    let temp = data;
+    temp.shift();
+    temp.push({Bitrate:6*num,time:new Date().toUTCString()});
+    if(6*num>max){
+        setMax(6*num);
+    }
+    setData(temp);
+    setRef(!ref);
+}
+	useEffect(()=>{
+	if(data.length)	setTimeout(setDate(),10000);
+	},[data,num,t]);
+
+	return(
+		<div>
+			<div style={{
+				display:'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				paddingTop:30
+			}}>
+        <ResponsiveContainer width={1200} height={600}>
+				<AreaChart
+					width={1200}
+					height={600}
+					data={data}
+					baseValue={'auto'}
+					margin={{
+						top: 10,
+						right: 30,
+						left: 0,
+						bottom: 0,
+					}}
+				>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey={"time"} tickFormatter={(e)=> `${new Date(e).getHours()}:${new Date(e).getMinutes()}`}/>
+					<YAxis width={80} tickFormatter={(e)=>e+' Mbit/s'}/>
+					<Tooltip formatter={(value, name, props) => [`${value} Mbit/s`]} labelFormatter={(value) =>new Date(value)} />
+          <ReferenceLine y={max} label={`Max ${max} Mb/s`} stroke="red" strokeDasharray="3 3" />
+					<Area type="monotone" dataKey="Bitrate" stroke={"rgb(176, 222, 9)"} fill="rgb(176, 222, 9)" />
+				</AreaChart>
+       
+        </ResponsiveContainer>
+
+			</div>
+			
+		</div>
+  )
+}
+
+export async function getServerSideProps({params,req,res,query,preview,previewData,resolvedUrl,locale,locales,defaultLocale}) {
+  console.log('Logging : '+res);
+  const data = await fetch('/api/getlist');
+  const list = await data.json();
+  return { props: { t:list.data } }
+}
---
+++ pages/value/index.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+import Slider,{SliderTooltip} from 'rc-slider';
+import 'rc-slider/assets/index.css';
+const {Handle} =Slider;
+const handle = props => {
+	const { value, dragging, index, ...restProps } = props;
+	return (
+  
+		<SliderTooltip
+			prefixCls="rc-slider-tooltip"
+			overlay={value}
+			visible={dragging}
+			placement="top"
+			key={index}
+		>
+			<Handle value={value} {...restProps} />
+		</SliderTooltip>
+    
+	);
+};
+const Value =()=>{
+    const [num, setNum] = useState(1)
+    const submit=(value)=>{
+        fetch("/api/value",{method:'POST',headers: {
+            'Content-Type': 'application/json'
+          },
+          body:JSON.stringify({value})})
+    }
+    return <div style={{display: 'flex',justifyContent:'center'}}>
+				<div style={{width: '300px',padding: 50}}>
+					<span style={{marginBottom:20,display: 'inline-block'}}>Value: {num}</span>
+					<Slider handle={handle}  min={0.1} max={8} step={0.03} onChange={(e)=>{
+						setNum(e);
+					}}
+                    onAfterChange={(e)=>submit(e)}
+					dots={true} value={num} defaultValue={1} startPoint={1} />
+				</div>  
+			</div>
+}
+export default Value
